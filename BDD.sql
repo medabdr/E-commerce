bfd1a81f700446162b8bcbe5946bf620cdd34021
medabdr <meds89601@gmail.com>
2026-03-29 19:54:34 +0100

    bdd and indexphp

---
+++ BDD.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS BDD;
+USE BDD;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    Role ENUM('admin', 'client') NOT NULL DEFAULT 'client'
+);
+CREATE TABLE produits (
+    id VARCHAR(255) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prix FLOAT NOT NULL,
+    image VARCHAR(255),
+    stock INT
+)
